e00211745ffd07a7636a9a7befbb2746f67341ae
Hugh Nowlan <nosmo@nosmo.me>
2020-09-19 17:40:43 +0100

    Read JSON input from stdin
    If valid JSON is piped into approxihash, use the JSON data as a
    key/value combination.
    
    Add some more general docs.

--- README.md
+++ README.md
@@ -21,6 +21,30 @@ to be valid, the same command can be run again with the ```-v``` flag
 to show what combination of elements and which hash function were used
 to generate the hash.
 
+Features
+-------
+
+*JSON input*: If the `-j`/`--json` parameter is passed on the
+ commandline, any input to STDIN will be treated as JSON. At the
+ moment the only supported data is a dictionary of key:value pairs.
+
+*Hash functions*: Approxihash supports all default hashlib functions
+ and these can be specified on the command line using
+ `--hashfn`/`-H`. A special `ALL` hash function is supported that will
+ generate all hashes with all hash functions.
+
+*Minimum combinations*: The `--min-combinations`/`-m` command line
+ argument specifies the minimum number of parameters used for
+ hashing - meaning that for 10 arguments with a minimum combination
+ factor of 3, only combinations of three or more elements from the 10
+ items in the argument list will be hashed.
+
+*Dividers*: By default the permutations of input data will not be
+ joined by a separator. The `--divider`/`-D` flag can be used to
+ specify a divider. Without a divider specified, hashed data for
+ `["a","b","c"]` will take combinations akin to `abc`. With a divider
+ specified, the combinations will be of the form `a,b,c,`.
+
 TODO
 -------
 ```TODO.org``` is tracking upcoming features and/or bugs.
--- approxihash.py
+++ approxihash.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import hashlib
 import sys
 import json
 import itertools
-
-from tests import test_data_hash
 
 AVAILABLE_HASH_TYPES = ["md5", "sha1", "sha256", "sha384", "sha512", "ALL"]
 
@@ -37,18 +35,19 @@ class GeneratedHash(object):
 
         """
         hash_results = {}
-        current_keys = self.data_dict.keys()
+        current_keys = list(self.data_dict.keys())
 
         # Use from min_combo_selections to the max number
-        for num_elements in range(min_combo_selections, len(self.data_dict.keys())+1):
+        for num_elements in range(min_combo_selections, len(list(self.data_dict.keys()))+1):
 
             # Get the combination of num_elements values
             for combination in itertools.permutations(current_keys, num_elements):
 
                 combo_fields = [ self.data_dict[elem] for elem in combination]
                 joined_combo = divider.join(combo_fields)
+
                 for hashfn in self.hash_types:
-                    combo_hash = hashlib.new(hashfn, joined_combo)
+                    combo_hash = hashlib.new(hashfn, joined_combo.encode("utf-8"))
                     hash_results[combo_hash.hexdigest()] = {"fields": combo_fields,
                                                             "hashfn": hashfn}
 
@@ -58,11 +57,11 @@ def main(hashfn, input_dict, min_combo_selection, divider, verbose):
 
     a = GeneratedHash(input_dict, hashfn)
     hashes = a.generate_hashes(divider, min_combo_selection)
-    for hash_str, hash_attrs in hashes.iteritems():
+    for hash_str, hash_attrs in hashes.items():
         if verbose:
-            print "%s %s %s" % (hash_str, ",".join(hash_attrs["fields"]), hash_attrs["hashfn"])
+            print("{} {} {}".format(hash_str, divider.join(hash_attrs["fields"]), hash_attrs["hashfn"]))
         else:
-            print hash_str
+            print(hash_str)
 
 if __name__ == "__main__":
 
@@ -87,10 +86,27 @@ if __name__ == "__main__":
 
     input_dict = {}
 
-    arg_data_dict = {}
-    for key, val in [ i.split(":") for i in args.keyvals ]:
-        arg_data_dict[key] = val
-    input_dict.update(arg_data_dict)
+    if args.json and not sys.stdin.isatty():
+
+        try:
+            input_dict.update(json.load(sys.stdin))
+        except ValueError as e:
+            sys.stderr.write("Failed to load input - is it valid JSON? {}\n".format(e))
+            sys.exit(254)
+
+        if not all([ type(i) == str or type(i) == int for i in input_dict.values() ]):
+            # for now any nested dicts or lists don't fit with our model
+            sys.stderr.write("Not all JSON keys are integers or strings - can't operate on this data\n")
+            sys.exit(253)
+
+        for key, val in input_dict.items():
+            if type(val) == int:
+                input_dict[key] = str(val)
+    else:
+        arg_data_dict = {}
+        for key, val in [ i.split(":") for i in args.keyvals ]:
+            arg_data_dict[key] = val
+        input_dict.update(arg_data_dict)
 
     main(args.hashfn, input_dict, args.min_combo_selection,
          args.divider, args.verbose)
